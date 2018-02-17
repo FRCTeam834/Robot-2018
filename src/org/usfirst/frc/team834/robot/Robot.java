c8ac80090bd607ac1cb8597490bbd9a13754167d
Krishna Dihora <24kjdih2@gmail.com>
2018-02-17 16:59:30 -0500

    Fixed Things

--- src/org/usfirst/frc/team834/robot/Robot.java
+++ src/org/usfirst/frc/team834/robot/Robot.java
@@ -49,8 +49,7 @@ public class Robot extends VisualRobot {
 	
 	//Other creations that do stuff
 	String aUselessVariable;
-	RumbleType kRightRumble;
-	RumbleType kLeftRumble;
+
 	//Other creations that do stuff
 	
 	
@@ -183,12 +182,17 @@ public class Robot extends VisualRobot {
 		catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
+		
+		
 		//Done choosing an auton and running it
+		
+		
 	}
-
+	
 	
 	
 	public void setLeftSide(double speed) {
+		
 		
 		//Used during build an auton to make sure speed doesn't go above 1
 		speed = speed > 1 ? 1 : speed;
@@ -202,151 +206,159 @@ public class Robot extends VisualRobot {
 	}
 
 	
+	
 	public void setRightSide(double speed) {
+		
+		
+		//Used during build an auton to make sure speed doesn't go above 1
 		speed = speed > 1 ? 1 : speed;
 		speed = speed < -1 ? -1 : speed;
+		//Used during build an auton to make sure speed doesn't go above 1
+		
 		
 		rightDrive.set(-speed);
+		
+		
 	}
 	
 
+	
 	@Override
 	public void teleOpInit() {
-
+		
+		
+		//There is nothing that must happen during teleOp Initialization
+		
+		
 	}
-
-
+	
+	
+	
 	@Override
 	public void teleOpPeriodic() {
 
-		// Makes joysticks control drivetrain
+		
+		//Allows the joysticks to control the robot driving
 		//This should be used used instead of independently setting the right and left side
 		driveTrain.tankDrive(-leftStick.getY(), -rightStick.getY());
-
-		// Makes xbox control elevator (-1 is up)
-		if (xbox.getRawButton(3)) { //X/Elevator Down
+		//Joysticks control robot ^^^
+		
+		
+		
+		//The xbox controller's X and Y buttons control the elevator
+		if (xbox.getRawButton(3)) { //X button
 			
-			elevator.set(1.0);
-			//elevator.set(1.0);
 			
-		} else if (xbox.getRawButton(4)) { //Y/Elevator Up
+			//The X button here causes the elevator to go down when it is pressed
+			elevator.set(1.0 /*The 1.0 causes the motors to spin, making the elevator to go down*/);
 			
-			if (limitElevatorTop.get()) {
+			
+		} 
+		
+		
+		else if (xbox.getRawButton(4)) { //Y button
+			
+			
+			//This code runs when the Y button is pressed
+			//The if statement below checks to see if the limit switch is pressed
+			if (!limitElevatorTop.get()) { //The limit switch displays true at all times but false when pressed
 				
-				elevator.set(-1.0);
-				
-			} else if (!limitElevatorTop.get()) {
-				
-				elevator.set(-1.0); //SET TO (-.1)
+				//Sets the elevator to go up when the limit switch is not pressed
+				elevator.set(-1.0);	
 				
 			}
 			
-			//elevator.set(-1.0);
 			
-		} else if (xbox.getRawButton(8)) { //  Start button stops elevator and holds in place from da gravityies
+			//If the elevator limit switch is pressed, then the below code is run
+			else if (limitElevatorTop.get()) {
+				
+				//The elevator is set to -0.1 when the limit switch is pressed, so the elevator stays up at the top
+				//but the motor does not keep running at max speed and get damaged
+				elevator.set(-0.1); //###THIS IS SET TO -1.0 RIGHT NOW BECAUSE LIMIT SWITCH DOES NOT WORK, SET TO -0.1 WHEN FIXED / FOR REAL ROBOT!!!####
+				
+			}}
+		
+		
+		else if (xbox.getRawButton(8)) { //Start button
 			
-			elevator.set(-.1);
+			//The start button on the xbox controller sets the motor to -0.1
+			//this holds the elevator in place, so it does not drop from gravity.
+			elevator.set(-0.1);
+		
 			
 		}
+		
 		
 		else {
+			
+			//This sets the elevator to stop moving if neither x or y are pressed
 			elevator.set(0);
+			//Setting the above value to -0.1 could be useful, but keeps the motor running the entire teleOp period.
 			
 		}
-
 		
-		//Button to pull in and push out the cube (2 is intakeIn)
-		if (xbox.getRawAxis(2) >= 0.75) { //Left Trigger
+		
+		
+		//These if statements cause the left and right triggers to control the motors to "swallow" and "spit out" the cubes
+		if (xbox.getRawAxis(2) >= 0.75) { //This is the Left Trigger
+			
+			//When the left trigger is pressed, the wheels spin inward, allowing the robot to swallow some cubes
 			intakeLeft.set(-1.0);
-			intakeRight.set(-1.0);
-
-			
-			
-		} else if (xbox.getRawAxis(3) >= 0.75) { //Right Trigger
-			intakeLeft.set(1.0);
 			intakeRight.set(1.0);
+						
+		}
+		
+		
+		else if (xbox.getRawAxis(3) >= 0.75) { //This is the right trigger
+			
+			//When the right trigger is pressed, the wheels spin outward, allowing the robot to spit out the cubes
+			intakeLeft.set(1.0);
+			intakeRight.set(-1.0);
 			
 			
-		} else {
+		}
+		
+		
+		else {
+			
+			//When neither trigger is pressed, the wheels for the intake are turned off.
 			intakeLeft.set(0);
 			intakeRight.set(0);
-		}
-		
-		//Testing the little spinny thingies on the xbox controller!
-		if (xbox.getRawAxis(0) >= 0.75) { //Left Trigger
-			xbox.setRumble(RumbleType.kLeftRumble, 1);
-			xbox.setRumble(RumbleType.kRightRumble, 1);
-			
-			
-		} else if (xbox.getRawAxis(1) >= 0.75) { //Right Trigger
-			xbox.setRumble(RumbleType.kLeftRumble, 1);
-			xbox.setRumble(RumbleType.kRightRumble, 1);
-
-			
-		} else {
-			xbox.setRumble(RumbleType.kLeftRumble, 0);
-			xbox.setRumble(RumbleType.kRightRumble, 0);
 			
 			
 		}
 		
-
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//Button to close or open the intake (5 is closed)
-		if (xbox.getRawButton(5)) { //Left Shoulder
+		//This is to control if the intake is open or closed
+		if (xbox.getRawButton(5)) { //This is the Left Shoulder Button
 			
+			//This checks /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			if (!limitIntake.get()) {
 				
-				xbox.setRumble(kLeftRumble, 1);
-				xbox.setRumble(kRightRumble, 1);
-				
+				xbox.setRumble(RumbleType.kLeftRumble, 1);
+				xbox.setRumble(RumbleType.kRightRumble, 1);				
 			} else {
-				xbox.setRumble(kLeftRumble, 0);
-				xbox.setRumble(kRightRumble, 0);
-				
-				if (xbox.getRawAxis(5) >= 75) {
-					
-					xbox.setRumble(kLeftRumble, 1);
-					xbox.setRumble(kRightRumble, 1);
-					
-				} else { 
-					
-					xbox.setRumble(kLeftRumble, 0);
-					xbox.setRumble(kRightRumble, 0);
-				}
+				xbox.setRumble(RumbleType.kLeftRumble, 0);
+				xbox.setRumble(RumbleType.kRightRumble, 0);
 			}
-			
 			intakeGrab.set(1.0);
-			
+
+							
 		} else if (xbox.getRawButton(6)) {//Right Shoulder
 		
-			
-			
 			if (!limitIntake.get()) {
 				
 				xbox.setRumble(RumbleType.kLeftRumble, 1);
 				xbox.setRumble(RumbleType.kRightRumble, 1);
-				
-				intakeGrab.set(-1.0);
-				
+
 			} else {
 				xbox.setRumble(RumbleType.kLeftRumble, 0);
 				xbox.setRumble(RumbleType.kRightRumble, 0);
-				
-				intakeGrab.set(-1.0);
+
 			}
 			
-			//intakeGrab.set(-1.0);
-			
+			intakeGrab.set(-1.0);
+
 			
 		} else {
 			
@@ -354,7 +366,7 @@ public class Robot extends VisualRobot {
 			intakeGrab.set(0);
 			xbox.setRumble(RumbleType.kLeftRumble, 0);
 			xbox.setRumble(RumbleType.kRightRumble, 0);
-			
+			/*
 			if (leftStick.getRawButton(7)) {
 				
 				xbox.setRumble(RumbleType.kLeftRumble, 1);
@@ -373,6 +385,7 @@ public class Robot extends VisualRobot {
 				xbox.setRumble(kRightRumble, 0);
 			
 			}
+			*/
 		}
 		
 		//Buttons that make your robot climb up and down
@@ -382,7 +395,7 @@ public class Robot extends VisualRobot {
 		else if(xbox.getRawButton(2 )){ //B/Climb Up
 			climber.set(-1.0); //Retract (Robot goes up)
 		}
-		else{
+		else {
 			climber.set(0);
 		}
 	/*	
